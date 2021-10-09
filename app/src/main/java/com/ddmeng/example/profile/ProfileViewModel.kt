925f9e8576ebae4284670d6628532884c2145385
Dandan Meng <mengdandanno1@163.com>
2021-10-09 12:06:16 +0800

    saved state handle to get arguments

--- app/src/main/java/com/ddmeng/example/profile/ProfileViewModel.kt
+++ app/src/main/java/com/ddmeng/example/profile/ProfileViewModel.kt
@@ -1,5 +1,7 @@
 package com.ddmeng.example.profile
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import com.ddmeng.example.StateViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -7,6 +9,15 @@ import javax.inject.Inject
 data class ProfileState(val id: Int = -1, val name: String = "")
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : StateViewModel<ProfileState>() {
+class ProfileViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle
+) : StateViewModel<ProfileState>() {
+
+    init {
+        val userId = savedStateHandle.get<Int>("userId")
+        val userName = savedStateHandle.get<String>("userName")
+        Log.i("savedStateHandle", "userId $userId userName $userName")
+    }
+
     override fun configureInitialState() = ProfileState()
 }
